5050a52f6bb06ccfff83e172df882611c80ae90f
MacRat <m@crat.jp>
2018-08-18 16:35:39 +0900

    Fixed grouping metrics

--- metrics.go
+++ metrics.go
@@ -22,14 +22,12 @@ func NewMetrics(info *StatusInfo) *Metrics {
 
 func PushToPrometheus(server *url.URL, metrics *Metrics) error {
 	gateway := push.New(server.String(), "updns")
+	gateway.Grouping("domain", metrics.domain)
 
 	if metrics.CurrentAddressTakenTime.Seconds() > 0 {
 		currentAddress := prometheus.NewGauge(prometheus.GaugeOpts{
 			Namespace: "updns",
 			Name: "current_address_get_seconds",
-			ConstLabels: prometheus.Labels{
-				"domain": metrics.domain,
-			},
 			Help: "time taken to obtain the current address that set into DNS server.",
 		})
 		currentAddress.Set(metrics.CurrentAddressTakenTime.Seconds())
@@ -40,9 +38,6 @@ func PushToPrometheus(server *url.URL, metrics *Metrics) error {
 		realAddress := prometheus.NewGauge(prometheus.GaugeOpts{
 			Namespace: "updns",
 			Name: "real_address_get_seconds",
-			ConstLabels: prometheus.Labels{
-				"domain": metrics.domain,
-			},
 			Help: "time taken to obtain the real IP address.",
 		})
 		realAddress.Set(metrics.RealAddressTakenTime.Seconds())
@@ -53,9 +48,6 @@ func PushToPrometheus(server *url.URL, metrics *Metrics) error {
 		update := prometheus.NewGauge(prometheus.GaugeOpts{
 			Namespace: "updns",
 			Name: "record_update_seconds",
-			ConstLabels: prometheus.Labels{
-				"domain": metrics.domain,
-			},
 			Help: "time taken to updating DNS record.",
 		})
 		update.Set(metrics.UpdateTakenTime.Seconds())
@@ -65,9 +57,6 @@ func PushToPrometheus(server *url.URL, metrics *Metrics) error {
 	lastUpdated := prometheus.NewGauge(prometheus.GaugeOpts{
 		Namespace: "updns",
 		Name: "last_updated_timestamp_seconds",
-		ConstLabels: prometheus.Labels{
-			"domain": metrics.domain,
-		},
 		Help: "DNS record last updated time.",
 	})
 	lastUpdated.Set(float64(metrics.LastUpdated.Unix()))
@@ -77,7 +66,6 @@ func PushToPrometheus(server *url.URL, metrics *Metrics) error {
 		Namespace: "updns",
 		Name: "executed_total",
 		ConstLabels: prometheus.Labels{
-			"domain": metrics.domain,
 			"type": "updated",
 		},
 		Help: "the count that updns was executed.",
@@ -89,7 +77,6 @@ func PushToPrometheus(server *url.URL, metrics *Metrics) error {
 		Namespace: "updns",
 		Name: "executed_total",
 		ConstLabels: prometheus.Labels{
-			"domain": metrics.domain,
 			"type": "not-updated",
 		},
 		Help: "the count that updns was executed.",
@@ -101,7 +88,6 @@ func PushToPrometheus(server *url.URL, metrics *Metrics) error {
 		Namespace: "updns",
 		Name: "error_total",
 		ConstLabels: prometheus.Labels{
-			"domain": metrics.domain,
 			"type": "minor",
 		},
 		Help: "the count that coused errors.",
@@ -113,7 +99,6 @@ func PushToPrometheus(server *url.URL, metrics *Metrics) error {
 		Namespace: "updns",
 		Name: "error_total",
 		ConstLabels: prometheus.Labels{
-			"domain": metrics.domain,
 			"type": "fatal",
 		},
 		Help: "the count that coused errors.",
